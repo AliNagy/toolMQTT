e84d563fc8ccb747893318107d940f15cf1ad2ff
Ali Nagy <alinagy50@gmail.com>
2018-06-30 11:48:48 +0200

    ~ Port edit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const port = 80
+const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/www'))
 
